a6867ea0bd7cafa348408c02a68e70c6c4fbf4de
Jonathan <jpat82792@gmail.com>
2018-03-30 05:16:17 -0500

    Add navbar-modal

--- public/javascripts/navbarController.js
+++ public/javascripts/navbarController.js
@@ -5,17 +5,19 @@ var initNavIcon = function(){
   document.getElementById('options-menu').addEventListener('click', function(){
     toggleOptionsMenu();
   });
+  document.getElementById('navbar-close-modal').addEventListener('click', function(event){
+    event.stopPropagation();
+    toggleOptionsMenu();
+  });
 }
 
 var toggleOptionsMenu = function(){
   console.log('toggleOptionsMenu()');
   var navbarModal = document.getElementById('navbar-modal');
   if(navbarModal.classList.contains('active-modal')){
-    //Close modal
     navbarModal.classList.remove('active-modal');
   }
   else{
-    //open modal
     navbarModal.classList.add('active-modal');
   }
 }
